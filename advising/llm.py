3574952a94291bd091d63623cfff48ce87cae481
HuginTheCrow <huginthecrow@proton.me>
2022-10-23 17:12:14 +0000

    drivers/tty/hvc: don't free hvc_console_setup after init

--- advising/llm.py
+++ advising/llm.py
@@ -12,3 +12,20 @@ from constants import AdvisorConstants
 
 class Advisor:
     """
+    Advisor class that prepares the reports by summarizing discoveries and providing attack advice,
+    leveraging GPT-4 models from OpenAI API.
+    """
+
+    def __init__(self,
+                 discovery_result,
+                 title,
+                 exploits,
+                 api_key=os.getenv('OPENAI_API_KEY'),
+                 api_url='https://api.openai.com/v1/chat/completions'):
+        
+        if not (api_key):
+            self.logger.error(AdvisorConstants.API_KEY_ERROR)
+            while not (api_key):
+                api_key = input("PLEASE ENTER OPENAI API KEY: ")
+        self.full_discovery_result = discovery_result
+        self.discovery_result = " ".join(discovery_result.split()[:2000])
