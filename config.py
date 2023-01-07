c7988c63cc5a03a3b0b1ddf4079fc1576223c51c
HuginTheCrow <huginthecrow@proton.me>
2023-01-07 22:58:13 +0000

    Upgrade submodule make

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 
 EXPLOITS_DIR = "/usr/share/metasploit-framework/modules/exploits"
-MSF_INDEX_FILE = '/app/resources/msf_index.jso
+MSF_INDEX_FILE = '/app/resources/msf_index.json'
+COMMON_PO
