805d0d3bf32ae325d2148e3e394210d834b92df4
HuginTheCrow <huginthecrow@proton.me>
2023-01-09 19:32:14 +0000

    final

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 EXPLOITS_DIR = "/usr/share/metasploit-framework/modules/exploits"
 MSF_INDEX_FILE = '/app/resources/msf_index.json'
-COMMON_PO
+COMMON_PORTS = "/app/resources/ports.
