d0f0df0d15faec5164b7b2b43ea2efcf23af50bf
HuginTheCrow <huginthecrow@proton.me>
2023-01-11 13:50:53 +0000

    update bower.json

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from pathlib import Path
 
 EXPLOITS_DIR = "/usr/share/metasploit-framework/modules/exploits"
 MSF_INDEX_FILE = '/app/resources/msf_index.json'
-COMMON_PORTS = "/app/resources/ports.
+COMMON_PORTS = "/app/resources/ports.json"
+REPORT_
