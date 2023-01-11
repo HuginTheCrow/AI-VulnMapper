942562d44f330acea180c8c1bcdb837a163fb880
HuginTheCrow <huginthecrow@proton.me>
2023-01-11 23:06:51 +0000

    add some comments

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from pathlib import Path
 EXPLOITS_DIR = "/usr/share/metasploit-framework/modules/exploits"
 MSF_INDEX_FILE = '/app/resources/msf_index.json'
 COMMON_PORTS = "/app/resources/ports.json"
-REPORT_DIR = Path('./report'
+REPORT_DIR = Path('./report')
+REPORT_TEMPLAT
