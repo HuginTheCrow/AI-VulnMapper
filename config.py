fc6c262314f3426f511246e55784a709104d2b29
HuginTheCrow <huginthecrow@proton.me>
2023-01-15 16:01:49 +0000

    Développeur backend Python/Django (H/F) en Stage à Paris

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ EXPLOITS_DIR = "/usr/share/metasploit-framework/modules/exploits"
 MSF_INDEX_FILE = '/app/resources/msf_index.json'
 COMMON_PORTS = "/app/resources/ports.json"
 REPORT_DIR = Path('./report')
-REPORT_TEMPLATE = '/app/webapp/templates/report_template.
+REPORT_TEMPLATE = '/app/webapp/templates/report_template.html'
+PROMPTS_FI
