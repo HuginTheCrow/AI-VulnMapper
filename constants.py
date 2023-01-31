a13fd2ec5805d4ea88fee4d67175ff0c18a6b240
HuginTheCrow <huginthecrow@proton.me>
2023-01-31 19:24:57 +0000

    Some clean up.

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 class AdvisorConstants:
     API_KEY_ERROR = "OPENAI_API_KEY NOT SET AS AN ENVIRONMENT VARIABLE"
-    ADVICE_ATTACK = "Advising attack
+    ADVICE_ATTACK = "Advising attack based on the discov
