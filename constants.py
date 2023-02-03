f9c93ca2702c69e4470a6212aefe87027321d250
HuginTheCrow <huginthecrow@proton.me>
2023-02-03 10:39:38 +0000

    spacing fixes

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 class AdvisorConstants:
     API_KEY_ERROR = "OPENAI_API_KEY NOT SET AS AN ENVIRONMENT VARIABLE"
     ADVICE_ATTACK = "Advising attack based on the discovery result."
-    
+    DISCOVERIES = 
