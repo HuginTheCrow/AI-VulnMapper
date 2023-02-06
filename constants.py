e80c1475605a9644de9f0dd0cbeae276dc749e48
HuginTheCrow <huginthecrow@proton.me>
2023-02-06 11:25:35 +0000

    travis.ci

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ class AdvisorConstants:
     API_KEY_ERROR = "OPENAI_API_KEY NOT SET AS AN ENVIRONMENT VARIABLE"
     ADVICE_ATTACK = "Advising attack based on the discovery result."
     DISCOVERIES = "Summarizing the discoveries."
-    HTML_REPORT = "Prep
+    HTML_REPORT = "Preparing the HTML repo
