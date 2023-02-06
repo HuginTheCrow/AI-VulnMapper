2a524a3fd3f4a55286f440d86b76158cc1abd9c4
HuginTheCrow <huginthecrow@proton.me>
2023-02-06 09:09:53 +0000

    chore(package): update chai to version 4.1.1

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 class AdvisorConstants:
     API_KEY_ERROR = "OPENAI_API_KEY NOT SET AS AN ENVIRONMENT VARIABLE"
     ADVICE_ATTACK = "Advising attack based on the discovery result."
-    DISCOVERIES = "Summarizing the disc
+    DISCOVERIES = "Summarizing the discoveries."
+    H
