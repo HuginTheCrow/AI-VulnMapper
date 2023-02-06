948a6f912ad3fca608d37a90dd46425b02f01547
HuginTheCrow <huginthecrow@proton.me>
2023-02-06 12:02:23 +0000

    Add arch tagline

--- constants.py
+++ constants.py
@@ -2,4 +2,6 @@ class AdvisorConstants:
     API_KEY_ERROR = "OPENAI_API_KEY NOT SET AS AN ENVIRONMENT VARIABLE"
     ADVICE_ATTACK = "Advising attack based on the discovery result."
     DISCOVERIES = "Summarizing the discoveries."
-    HTML_REPORT = "Preparing the HTML repo
+    HTML_REPORT = "Preparing the HTML report."
+
+class ExploitMa
