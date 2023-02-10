6b1aec7a21895133a05d651a6d34702912f23386
HuginTheCrow <huginthecrow@proton.me>
2023-02-10 20:18:28 +0000

    minor improvments

--- constants.py
+++ constants.py
@@ -7,4 +7,5 @@ class AdvisorConstants:
 class ExploitManagerConstants:
     START_MSF = "Starting MSF..."
     MSF_SUCCESSFUL = "MSF successfully started."
-    MSF_ERROR = "MSF failed to start
+    MSF_ERROR = "MSF failed to start!"
+    MSF_TIMEDOUT
