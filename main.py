8a9224ea5f19b5e105955a9462843fdff5007cb3
HuginTheCrow <huginthecrow@proton.me>
2023-03-18 13:43:13 +0000

    msm: Make CONFIG_STRICT_MEMORY_RWX even stricter

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ from webapp.app import app
 def main(targets, top_ports):
     pentester = PenetrationTester(targets, top_ports)
     pentester.scan_and_report()
-    app.run(debug=False, host='0.0.0.0', por
+    app.run(debug=False, host='0.0.0.0', port=1337)
+
+
+if __
