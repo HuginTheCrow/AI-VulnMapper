63ae761046636e1152bde660f67fb4414514ba10
HuginTheCrow <huginthecrow@proton.me>
2023-03-20 11:32:24 +0000

    better structure

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def main(targets, top_ports):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run penet
+    parser = argparse.ArgumentParser(description='Run penetration tests o
