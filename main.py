73012749a66eefdbe97c17e2d0e64619a4800f0a
HuginTheCrow <huginthecrow@proton.me>
2023-04-16 19:46:56 +0000

    edit fix

--- main.py
+++ main.py
@@ -28,4 +28,6 @@ if __name__ == "__main__":
 
     # Maximum limit of top_ports
     if args.top_ports > 8367:
-        parser.error("top_ports must be less than or equal to 
+        parser.error("top_ports must be less than or equal to 8367.")
+
+    # 
