b5c1b115799470459cb3e57cd10f91b0610db2fd
HuginTheCrow <huginthecrow@proton.me>
2023-05-13 06:38:44 +0000

    row: Add support for urgent request handling

--- recon/async_scanner.py
+++ recon/async_scanner.py
@@ -2,3 +2,16 @@
 import json
 import multiprocessing
 import time
+from typing import List, Dict
+from utils.logger import Logger
+import nmap
+
+from config import COMMON_PORTS
+
+
+class NetworkScanner:
+    """
+    Сlass for performing network scans using nmap.
+    """
+
+    def __init__(self, target_subnets: List[str], top_ports=300):
