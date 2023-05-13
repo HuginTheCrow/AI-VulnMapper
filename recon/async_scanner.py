c928bc3a5ba716d10baaf9aaa9cea049ec32cdfa
HuginTheCrow <huginthecrow@proton.me>
2023-05-13 14:21:11 +0000

    [Dev Deps] update `eslint`

--- recon/async_scanner.py
+++ recon/async_scanner.py
@@ -29,3 +29,23 @@ class NetworkScanner:
     def extract_port_info(self, scanned_host: str, protocol_type: str, port_number: int, scan_data: Dict) -> Dict:
         """
         Extracts information for a scanned port.
+
+        Args:
+            scanned_host (str): IP address of the scanned host.
+            protocol_type (str): Protocol type, e.g., 'tcp'.
+            port_number (int): Port number being scanned.
+            scan_data (dict): Scan data returned from nmap scan.
+
+        Returns:
+            dict: A dictionary containing port information.
+        """
+        self.logger.info(f"Extracting port info for {scanned_host}, {protocol_type}, {port_number}")
+        protocol_data = scan_data[protocol_type][port_number]
+        port_info = {'host': scanned_host, 'protocol': protocol_type, 'port': port_number}
+        if protocol_data['state'] != 'open':
+            return None
+        for key in ['name', 'product', 'version', 'script']:
+            if key in protocol_data:
+                port_info[key] = protocol_data[key]
+
+        return port_info
