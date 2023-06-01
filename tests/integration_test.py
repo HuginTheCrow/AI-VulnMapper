d3118b7f8c98042011bce30ed5ca3af009714d66
HuginTheCrow <huginthecrow@proton.me>
2023-06-01 14:10:42 +0000

    Some formatting fixes

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -7,4 +7,4 @@ import time
 def is_port_open(host, port, timeout=1):
     """Check if a given port is open."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
+        s.settimeout(tim
