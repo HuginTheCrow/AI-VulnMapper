d6ec84328aee6ed5dcde9a1c4201fbf6f49ab3c9
HuginTheCrow <huginthecrow@proton.me>
2023-11-08 11:39:42 +0000

    typing

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -25,4 +25,4 @@ def test_server_start():
     3. Checks for 60 seconds to see if the server starts and becomes accessible on '127.0.0.1:1337'.
     4. If the server starts within the duration, the test is considered successful.
     5. If the server doesn't start within 60 seconds, or if the Python script encounters an issue,
-       the te
+       the test is marked as faile
