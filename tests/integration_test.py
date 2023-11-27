94f5c3e03454d9c74933ad54c3e79252353ee00e
HuginTheCrow <huginthecrow@proton.me>
2023-11-27 19:47:06 +0000

    Shaders

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -31,4 +31,4 @@ def test_server_start():
         None. If the server doesn't start as expected, an assertion error is raised.
     """
     # Run the python script in the background
-    process = 
+    process = subprocess.Popen(["pytho
