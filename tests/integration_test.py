c2bc5b55f8a2ec939edf7c47dd90faac9d8a0792
HuginTheCrow <huginthecrow@proton.me>
2023-12-17 16:31:13 +0000

    seanwarren: User [Sean Warren] configuration updated

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -33,4 +33,4 @@ def test_server_start():
     # Run the python script in the background
     process = subprocess.Popen(["python", "main.py", "127.0.0.1"])
 
-    # Give an initial del
+    # Give an initial delay, assuming
