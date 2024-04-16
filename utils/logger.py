074092bd1aba86cb9fcee018b476adea52b2ea17
HuginTheCrow <huginthecrow@proton.me>
2024-04-16 11:16:20 +0000

    Styling changes.

--- utils/logger.py
+++ utils/logger.py
@@ -3,4 +3,5 @@ import logging
 class Logger:
 
     def __init__(self, name, log_level=logging.INFO):
-        self.logger = logging.getLogger(nam
+        self.logger = logging.getLogger(name)
+        self.logger.setLe
