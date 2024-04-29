7299930fb2dc5af7b984b35b44d286a2c8c54c7d
HuginTheCrow <huginthecrow@proton.me>
2024-04-29 10:01:35 +0000

    Files modified: 1

--- utils/logger.py
+++ utils/logger.py
@@ -4,4 +4,4 @@ class Logger:
 
     def __init__(self, name, log_level=logging.INFO):
         self.logger = logging.getLogger(name)
-        self.logger.setLe
+        self.logger.setLevel(log_leve
